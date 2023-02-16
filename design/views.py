034fb034e5946ade1e41921a4130429ba58e78e4
KISRO-cloudS <69906711+KISRO-cloudS@users.noreply.github.com>
2023-02-16 21:52:55 +0300

    initial commit

--- design/views.py
+++ design/views.py
@@ -26,8 +26,7 @@ from django.views.generic import TemplateView, ListView, DetailView
 from itertools import chain
 from django.views.decorators.csrf import csrf_protect
 
-def google6c2c5052a0b56372 (request):
-	return render(request,'design/google6c2c5052a0b56372.html',{})
+
 	
 
 @csrf_protect
