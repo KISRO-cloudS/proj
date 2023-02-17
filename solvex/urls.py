d62944c54184c7e7dbbc4650e4a0095565adf513
KISRO-cloudS <69906711+KISRO-cloudS@users.noreply.github.com>
2023-02-17 09:22:44 +0300

    initial commit

--- solvex/urls.py
+++ solvex/urls.py
@@ -223,5 +223,3 @@ if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
 	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
-
