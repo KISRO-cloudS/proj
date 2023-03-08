c667a1be0f90d098b5244c30d1c4da4040e77692
KISRO-cloudS <69906711+KISRO-cloudS@users.noreply.github.com>
2023-03-08 10:56:59 +0300

    initial commit

--- solvex/wsgi.py
+++ solvex/wsgi.py
@@ -8,7 +8,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solvex.settings')
 application = get_wsgi_application()
 
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
